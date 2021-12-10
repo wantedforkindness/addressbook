a2be02aad94d2ebea7009e5a02b797f67e3b06f1
wantedforkindness <94136220+wantedforkindness@users.noreply.github.com>
2021-12-10 10:04:02 -0800

    Update ContactService.java

--- src/main/java/com/vaadin/tutorial/addressbook/backend/ContactService.java
+++ src/main/java/com/vaadin/tutorial/addressbook/backend/ContactService.java
@@ -19,8 +19,8 @@ public class ContactService {
     static String[] fnames = { "Peter", "Alice", "John", "Mike", "Olivia",
             "Nina", "Alex", "Rita", "Dan", "Umberto", "Henrik", "Rene", "Lisa",
             "Linda", "Timothy", "Daniel", "Brian", "George", "Scott",
-            "Jennifer", "Abi", "Rose", "lucy", "reigns", "David" };
-    static String[] lnames = { "Smith", "Johnson", "Williams", "Jones",
+            "Jennifer", "Abi", "Rose", "lucy", "reigns", "David", "kelly" };
+    static String[] lnames = { "Smith", "Johnson", "Williams", "Jones", "Precious",
             "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor",
             "Anderson", "Thomas", "Jackson", "White", "Harris", "Martin",
             "Thompson", "Young", "King", "Robinson", "Ego", "Ago", "ego", "Ali", "asong"};
